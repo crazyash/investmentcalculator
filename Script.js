8c8f0546fd2ddf436289d88df1039c66896a441b
Ashok Kumar Bandige <ashokkumar.bandige@colruytgroup.com>
2019-01-15 13:43:23 +0100

    Added SIP Calculator

--- Script.js
+++ Script.js
@@ -1,9 +1,10 @@
 /*FD Calculation*/
-function caclulateFD(){
-	var P=document.getElementById('principleamount').value;
+function calculateFD(){
+	var P=document.getElementById('principleamount').value.replace(/,/g, "");
 	var r=document.getElementById('interestrate').value;
 	var tt=document.getElementById('tenureType').value;
 
+	
 	if(tt=='m')
 		var t=document.getElementById('tenure').value/12;
 	else
@@ -18,8 +19,8 @@ function caclulateFD(){
 }
 
 /*RD Calculation*/
-function caclulateRD(){
-	var P=document.getElementById('installment').value;
+function calculateRD(){
+	var P=document.getElementById('installment').value.replace(/,/g, "");
 	var r=document.getElementById('interestrate').value;
 	var n=document.getElementById('frequency').value;
 	var t=(document.getElementById('durationY').value*12)+parseInt(document.getElementById('durationM').value);
@@ -35,6 +36,27 @@ function caclulateRD(){
 	document.getElementById('rdresult').hidden=false;
 	document.getElementById('rdresult').innerHTML="<table><tr><td>Maturity Amount</td><td>: Rs. "+ amount.toLocaleString('en-IN')+"</td></tr><tr><td>Total Interest Earned</td><td>: Rs. "+ interest.toLocaleString('en-IN')+"</td></tr></table>";
 }
+
+/*SIP Calculation*/
+function calculateSIP(){
+	var P=document.getElementById('installment').value.replace(/,/g, "");
+	var r=document.getElementById('interestrate').value;
+	var n=1;
+	var t=(document.getElementById('durationY').value*12)+parseInt(document.getElementById('durationM').value);
+
+	var amount=0;
+	 for (var i = t; i>=1; i--){
+		 amount=amount + P*Math.pow(1+((r/100)/n),n*i/12);
+     }
+
+	amount=Math.round(amount);
+	var interest=amount-(P*t);
+	
+	document.getElementById('sipresult').hidden=false;
+	document.getElementById('sipresult').innerHTML="<table><tr><td>Maturity Amount</td><td>: Rs. "+ amount.toLocaleString('en-IN')+"</td></tr><tr><td>Total Interest Earned</td><td>: Rs. "+ interest.toLocaleString('en-IN')+"</td></tr></table>";
+}
+
+
 
 function toINR(money){
 	var x= document.getElementById(money);
